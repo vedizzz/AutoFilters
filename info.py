95350d26e4da698fe0eed946a7dd6017e95437e7
vedizzz <nithishtk6@gmail.com>
2025-04-20 16:07:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,8 +91,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/How_to_Download_7x/30
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'ea7dec972a0670994ad2f4e6be0dc10688fb8db4')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'softurl.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'aedb193fbece0c8370b8535e5ba8a878b4804548')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Tamil_LinksZzz/36')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
